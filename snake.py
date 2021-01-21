57e02da0e40fb2c83571312e3f7eafec5d1aec64
Axel <axosar@gmail.com>
2021-01-21 09:37:40 -0600

    Food collide

--- snake.py
+++ snake.py
@@ -29,17 +29,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.head.setheading() != DOWN:
+        if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
     def down(self):
-        if self.head.setheading() != UP:
+        if self.head.heading() != UP:
             self.head.setheading(DOWN)
 
     def left(self):
-        if self.head.setheading() != RIGHT:
+        if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
-        if self.head.setheading() != LEFT:
+        if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
