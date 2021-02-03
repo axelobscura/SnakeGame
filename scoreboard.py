ccd590ff63bb69c668e5ae0c109a8c69b40ff657
Axel <axosar@gmail.com>
2021-02-03 08:43:43 -0600

    Adding high score tag

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, -270)
@@ -14,13 +15,19 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"SCORE: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.increase_score()
+
+    #def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
